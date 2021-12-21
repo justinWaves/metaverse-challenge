8d2db1cba9b194742a4e80482415a1f11034b2fc
justinWaves <juhstinn@gmail.com>
2021-12-20 16:44:48 -0800

    built chat fuctionality

---
+++ components/Avatar.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
+
+function Avatar({ username, logoutOnPress }) {
+  const { user, logout } = useMoralis();
+
+  return (
+    <Image
+      className="rounded-full bg-black cursor-pointer"
+      src={`https://avatars.dicebear.com/api/miniavs/${
+        username || user.get("username")
+      }.svg`}
+      onClick={() => logoutOnPress && logout()}
+      layout="fill"
+    />
+  );
+}
+
+export default Avatar;
